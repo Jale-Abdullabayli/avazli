dae622a31ab10f4cd45e17d2528bba8925ccb743
iMac <imac@iMacs-iMac.local>
2021-06-08 17:57:07 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+
+ AOS.init();
+
+var windowHeight=$(window).height();
+
+$(window).scroll(function (event) {
+var top =$(window).scrollTop();
+    if (top <= windowHeight) {
+        console.log(top)
+       $('.caption .container').css('transform',`translateY(${top*0.4}px)`)
+    }
+})
+
+
+//clock
+
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
